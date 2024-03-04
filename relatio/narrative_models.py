8231211bc8f3637f84ed1371b3bbea0924c36989
Leonidas Boutsikaris <leobouts@gmail.com>
2024-03-05 00:26:57 +0200

    Update narrative_models.py

--- relatio/narrative_models.py
+++ relatio/narrative_models.py
@@ -186,16 +186,11 @@ class NarrativeModel:
             # temp_counter = count_values(srl_res, keys=[role])
             # counter_for_phrases = counter_for_phrases + temp_counter
             # phrases = list(temp_counter)
-            print(srl_res)
+            
             phrases = []
-            for item in srl_res:
-                print(item)
-                for verb_data in item['verbs']:
-                    description = verb_data['description']
-                    pattern = r'\[' + role + r': (.*?)\]'
-                    match = re.search(pattern, description)
-                    if match:
-                        phrases.append(match.group(1))
+            for item in slr_res:
+                if role in item:
+                phrases.append(item[role])
                     
             # Remove known entities for the training of unknown entities
             if role in self.roles_with_known_entities:
